1cc56396f63f804f9c764c6c4131cc1e38a8eb64
s20zvinmart <s20zvinmart@vea.lv>
2023-07-25 15:24:50 +0300

    changeDestinationToIndia() + reusable text assertion in element method

--- src/test/java/org/selenide/examples/BtaTest.java
+++ src/test/java/org/selenide/examples/BtaTest.java
@@ -12,14 +12,31 @@ public class BtaTest {
     public void OpenBta(){
         open("https://www.bta.lv/");
         $("#module-284 > div > div.buttons > button:nth-child(3)").click(); // accept cookies
-        //sleep(3000);
     }
 
     @Test
     public void clickOnCelojumi(){
         OpenBta();
         $("#module-28 > div > div > div.list > a:nth-child(2)").click();
-        sleep(3000);
+    }
+
+    @Test
+    public void changeDestinationToIndia(){
+        clickOnCelojumi();
+        $("#regionalSelectorRegion-open > span.text.text-icon").click();
+        $("#regionalSelectorCountry-showListSearch").click();
+        $("#regionalSelectorCountry > div.popups > aside > div > div.elements > div > div.selected-countries").click();
+        $("#regionalSelectorCountry-addCountry > span").click();
+        $(By.xpath("//*[text()='Indija']")).click();
+        $("#regionalSelectorCountry-removeCountry-0").shouldHave(text("Indija"));
+        $("#regionalSelectorCountry-applyButton").click();
+        assertElementText("#regionalSelectorRegion-open", "Visa Pasaule");
+    }
+
+    // reusable methods
+
+    private void assertElementText(String elementId, String expectedText) {
+        $(elementId).shouldHave(text(expectedText));
     }
 
 }
