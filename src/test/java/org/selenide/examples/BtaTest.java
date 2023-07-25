df9f0e0c5c41515e2440138674e78e4524901099
s20zvinmart <s20zvinmart@vea.lv>
2023-07-25 16:02:42 +0300

    changeActivity() + other adjustments/refactoring

--- src/test/java/org/selenide/examples/BtaTest.java
+++ src/test/java/org/selenide/examples/BtaTest.java
@@ -15,14 +15,14 @@ public class BtaTest {
     }
 
     @Test
-    public void clickOnCelojumi(){
+    public void OpenCelojumi(){
         OpenBta();
         $("#module-28 > div > div > div.list > a:nth-child(2)").click();
     }
 
     @Test
     public void changeDestinationToIndia(){
-        clickOnCelojumi();
+        OpenCelojumi();
         $("#regionalSelectorRegion-open > span.text.text-icon").click();
         $("#regionalSelectorCountry-showListSearch").click();
         $("#regionalSelectorCountry > div.popups > aside > div > div.elements > div > div.selected-countries").click();
@@ -30,13 +30,27 @@ public class BtaTest {
         $(By.xpath("//*[text()='Indija']")).click();
         $("#regionalSelectorCountry-removeCountry-0").shouldHave(text("Indija"));
         $("#regionalSelectorCountry-applyButton").click();
-        assertElementText("#regionalSelectorRegion-open", "Visa Pasaule");
+        $("#regionalSelectorRegion-open > span.text.text-icon").shouldHave(text("Visa pasaule"));
+        $("#regionalSelectorRegion-open .text").shouldHave(text("Visa pasaule"));
+    }
+
+    @Test
+    public void changeActivity(){
+        OpenCelojumi();
+        clickElement("#travelActivities-open > span.text.text-icon");
+        clickElement("#travelActivities-popup-select-option-3");
+        assertElementText("#travelActivities-open", "Sports");
+        $("#form-travel-insurance > div:nth-child(7)").shouldBe(visible);
+        sleep(3000);
     }
 
     // reusable methods
 
-    private void assertElementText(String elementId, String expectedText) {
-        $(elementId).shouldHave(text(expectedText));
+    private void clickElement(String elementId) {
+        $(elementId).click();
     }
 
+    private void assertElementText(String selector, String expectedText) {
+        $(selector).shouldHave(text(expectedText));
+    }
 }
