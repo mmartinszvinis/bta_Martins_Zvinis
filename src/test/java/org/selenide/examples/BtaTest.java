4c94ad6e78097c4170ca63af7cdeb12810d87893
s20zvinmart <s20zvinmart@vea.lv>
2023-07-25 14:31:54 +0300

    clickOnCelojumi()

--- src/test/java/org/selenide/examples/BtaTest.java
+++ src/test/java/org/selenide/examples/BtaTest.java
@@ -1,15 +1,24 @@
 package org.selenide.examples;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class BtaTest {
 
     @Test
-    public void userCanOpenBta(){
+    public void OpenBta(){
         open("https://www.bta.lv/");
+        $("#module-284 > div > div.buttons > button:nth-child(3)").click(); // accept cookies
+        //sleep(3000);
+    }
+
+    @Test
+    public void clickOnCelojumi(){
+        OpenBta();
+        $("#module-28 > div > div > div.list > a:nth-child(2)").click();
         sleep(3000);
     }
 
