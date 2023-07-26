0e57340ee9d99abb4ff19b1e991fd4a810751ca7
s20zvinmart <s20zvinmart@vea.lv>
2023-07-26 14:01:24 +0300

    refactoring + all other steps and methods

--- src/test/java/org/selenide/examples/BtaTest.java
+++ src/test/java/org/selenide/examples/BtaTest.java
@@ -1,47 +1,143 @@
 package org.selenide.examples;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BtaTest {
 
-    @Test
-    public void OpenBta(){
+   @Before
+    public void setup(){
         open("https://www.bta.lv/");
-        $("#module-284 > div > div.buttons > button:nth-child(3)").click(); // accept cookies
+        System.out.println("bta.lv should be opened");
+        try{
+            boolean cookiesPromptIsDisplayed = $("#module-284 > div").isDisplayed();
+            if(cookiesPromptIsDisplayed){
+                clickElement("#module-284 > div > div.buttons > button:nth-child(3)"); // accept cookies
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
+    public void traverPoliciesFullFlow(){
+        OpenCelojumi();
+
+        changeDestinationToIndia();
+        changeActivitytoSport();
+        clickOnGetOffers();
+
+        offerScreenIsDisplayed();
+        openOptimalPlusDetails();
+        optimalPlusDetailsAreDisplayed();
+        closeOptimalPlusDetails();
+        selectOptimalPlusAndContinue();
+
+        travelerDataInputScreenIsDisplayed();
+        travelerDataInputFieldsAreDisplayed();
+        travelerDataInputFieldsAreEmpty();
+    }
+
+    // home screen
+
     public void OpenCelojumi(){
-        OpenBta();
-        $("#module-28 > div > div > div.list > a:nth-child(2)").click();
+        clickElement("#module-28 > div > div > div.list > a:nth-child(2)");
     }
 
-    @Test
+    // celojumi screen
+
     public void changeDestinationToIndia(){
-        OpenCelojumi();
-        $("#regionalSelectorRegion-open > span.text.text-icon").click();
-        $("#regionalSelectorCountry-showListSearch").click();
-        $("#regionalSelectorCountry > div.popups > aside > div > div.elements > div > div.selected-countries").click();
-        $("#regionalSelectorCountry-addCountry > span").click();
+        clickElement("#regionalSelectorRegion-open > span.text.text-icon");
+        clickElement("#regionalSelectorCountry-showListSearch");
+        clickElement("#regionalSelectorCountry > div.popups > aside > div > div.elements > div > div.selected-countries");
+        clickElement("#regionalSelectorCountry-addCountry > span");
         $(By.xpath("//*[text()='Indija']")).click();
-        $("#regionalSelectorCountry-removeCountry-0").shouldHave(text("Indija"));
-        $("#regionalSelectorCountry-applyButton").click();
-        $("#regionalSelectorRegion-open > span.text.text-icon").shouldHave(text("Visa pasaule"));
-        $("#regionalSelectorRegion-open .text").shouldHave(text("Visa pasaule"));
+
+        assertElementText("#regionalSelectorCountry-removeCountry-0 .text", "Indija");
+
+        clickElement("#regionalSelectorCountry-applyButton");
+        assertElementText("#regionalSelectorRegion-open .text", "Visa pasaule");
     }
 
-    @Test
-    public void changeActivity(){
-        OpenCelojumi();
-        clickElement("#travelActivities-open > span.text.text-icon");
-        clickElement("#travelActivities-popup-select-option-3");
-        assertElementText("#travelActivities-open", "Sports");
-        $("#form-travel-insurance > div:nth-child(7)").shouldBe(visible);
-        sleep(3000);
+    public void changeActivitytoSport(){
+       clickElement("#travelActivities-open > span.text.text-icon");
+       clickElement("#travelActivities-popup-select-option-3");
+       assertElementText("#travelActivities-open .text", "Sports");
+       $("#form-travel-insurance > div:nth-child(7)").shouldBe(visible);
+    }
+
+    public void clickOnGetOffers(){
+       clickElement("#form-travel-insurance > div:nth-child(8) > div > button");
+    }
+
+    // offers screen
+
+    public void offerScreenIsDisplayed(){
+       $("#module-888 > div > div > section.header").shouldBe(visible);
+       assertElementText("#module-888 > div > div > section.header", "Izvēlies programmu");
+    }
+
+    public void openOptimalPlusDetails(){
+        clickElement("#form-travel-policy > section > div:nth-child(2) > button.button-covered-popup");
+    }
+
+    public void closeOptimalPlusDetails(){
+        clickElement("#page-one-travel > div.popups > aside > div > button");
+    }
+
+    public void optimalPlusDetailsAreDisplayed(){
+       $("#page-one-travel > div.popups > aside > div").shouldBe(visible);
+    }
+
+    public void selectOptimalPlusAndContinue(){
+       clickElement("#form-travel-policy > section > div:nth-child(2) > button.button.hide-on-mobile.red");
+    }
+
+    // traveler data input screen
+
+    public void travelerDataInputScreenIsDisplayed(){
+       assertElementText("#form-travel-travellers > div.header", "Ceļotāju dati");
+    }
+
+    public void travelerDataInputFieldsAreDisplayed(){
+        nameInputFieldIsDisplayed();
+        surnameInputFieldIsDisplayed();
+        identityNumberInputFieldIsDisplayed();
+    }
+
+    public void nameInputFieldIsDisplayed(){
+       $("#travelerFirstName0").shouldBe(visible);
+    }
+
+    public void surnameInputFieldIsDisplayed(){
+        $("#travelerLastName0").shouldBe(visible);
+    }
+
+    public void identityNumberInputFieldIsDisplayed(){
+        $("#travelerIdentityNumber0").shouldBe(visible);
+    }
+
+    public void travelerDataInputFieldsAreEmpty(){
+        nameInputFieldIsEmpty();
+        surnameInputFieldIsEmpty();
+        identityNumberInputFieldIsEmpty();
+    }
+
+    public void nameInputFieldIsEmpty(){
+       $("#travelerFirstName0-text").shouldBe(empty);
+    }
+
+    public void surnameInputFieldIsEmpty(){
+        $("#travelerLastName0-text").shouldBe(empty);
+    }
+
+    public void identityNumberInputFieldIsEmpty(){
+        $("#travelerIdentityNumber0-text").shouldBe(empty);
     }
 
     // reusable methods
@@ -51,6 +147,8 @@ public class BtaTest {
     }
 
     private void assertElementText(String selector, String expectedText) {
-        $(selector).shouldHave(text(expectedText));
+        String actualText = $(selector).getText();
+        assertEquals(expectedText, actualText, "Expected text is not equal to actual text!");
     }
+
 }
