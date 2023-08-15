ff980ecc05ca9ca46690276a79e1120275e541d2
s20zvinmart <s20zvinmart@vea.lv>
2023-08-15 14:12:12 +0300

    adding functionality like sending all new listings and no listing sent when launched

--- src/test/java/org/selenide/examples/SsTest.java
+++ src/test/java/org/selenide/examples/SsTest.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.SelenideElement;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,36 +19,49 @@ public class SsTest {
     @Before
     public void setup() {
         Configuration.headless = true;
-        open("https://www.ss.lv/lv/real-estate/flats/ventspils-and-reg/ventspils/hand_over/");
-        System.out.println("ss should be opened");
+        open("https://www.ss.lv/lv/real-estate/flats/ventspils-and-reg/ventspils/");
+
+/*        open("https://www.ss.lv/lv/transport/cars/volkswagen/golf-7/filter/");
+        $("#f_o_35").click();
+        $("#f_o_35 > option:nth-child(2)").click();*/
+
+
+/*        open("https://www.ss.lv/lv/real-estate/flats/ventspils-and-reg/ventspils/hand_over/");
+        System.out.println("ss should be opened");*/
 
 
 /*        open("https://www.ss.lv/lv/transport/cars/");
         $("#filter_tbl > tbody > tr > td:nth-child(2) > input").click();*/
     }
 
-    private String lastSeenId = "#tr_53313754";
+    private Set<String> seenIds = new HashSet<>();
     private final String baseUrl = "https://api.telegram.org/bot6328543239:AAHFw0238VTkGd6MCIf67Q_3vLIv380kj7o/sendMessage";
 
     @Test
     public void monitorNewElement() throws InterruptedException {
+        // Initial capture of all current listings' ids without sending messages
+        initializeSeenIds();
+
         while (true) {
             try {
-                SelenideElement element = $("tr[id^='tr_']");
-                String currentId = element.getAttribute("id");
+                for (SelenideElement element : $$("tr[id^='tr_']")) {
+                    String currentId = element.getAttribute("id");
 
-                assert currentId != null;
-                if (!currentId.equals(lastSeenId)) {
-                    lastSeenId = currentId;
-                    String link = element.$("a.am").getAttribute("href");
-                    sendMessage("New listing found: " + link);
+                    // Check if the element has the "a.am" link before accessing it
+                    if (element.$$("a.am").size() > 0) {
+                        String link = element.$("a.am").getAttribute("href");
+                        if (!seenIds.contains(currentId)) {
+                            seenIds.add(currentId);
+                            sendMessage("New listing found: " + link);
+                        }
+                    }
                 }
             } catch (Exception e) {
                 // Handle exception
                 e.printStackTrace();
+                System.out.println("Error occurred while monitoring. Retrying...");
             }
-            //Thread.sleep(60000);// wait 1 min
-            Thread.sleep(30000);
+            Thread.sleep(60000); // wait 1 min
             refresh(); // Selenide refresh command
         }
     }
@@ -69,5 +84,13 @@ public class SsTest {
             }
         }
     }
+
+    private void initializeSeenIds() {
+        for (SelenideElement element : $$("tr[id^='tr_']")) {
+            String currentId = element.getAttribute("id");
+            seenIds.add(currentId);
+        }
+    }
+
 
 }
